c743e8bb96512a9c0007f4500d7f3b7fba4bffa8
senanur <senanurpnr@gmail.com>
2026-04-30 10:55:55 +0300

    Network sınıfı dinamik IP için güncellendi

--- client/network.py
+++ client/network.py
@@ -1,13 +1,26 @@
 import socket
 
 class Network:
-    def __init__(self, ip, port):
+    def __init__(self, server_ip):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((ip, int(port)))
+        self.server = server_ip # LoginWindow'dan gelen IP
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.player_id = self.connect()
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            # Bağlantı başarılıysa sunucu oyuncu numarasını (0 veya 1) gönderir
+            return self.client.recv(2048).decode()
+        except Exception as e:
+            print(f"Bağlantı hatası: {e}")
+            return None
 
     def send(self, data):
         try:
-            self.client.send(str(data).encode())
-            # return self.client.recv(1024).decode()#
-        except Exception as e:
-           print(f"Bağlantı hatası:{e}")
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
+            return None
